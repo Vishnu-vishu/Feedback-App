7d9cbf480599f0b68be2a886ddea295a8d26dc62
vishnu <vishnuvishnu9177@gmail.com>
2024-01-18 06:03:08 +0000

    solution

---
+++ src/components/Feedback/index.js
@@ -0,0 +1,58 @@
+// Write your React code here.
+import {Component} from 'react'
+
+import './index.css'
+
+class Feedback extends Component {
+  state = {changeFeedback: true}
+
+  showResult = () => {
+    this.setState({
+      changeFeedback: false,
+    })
+  }
+
+  render() {
+    const {changeFeedback} = this.state
+    const {resources} = this.props
+    const {emojis, loveEmojiUrl} = resources
+    return (
+      <div className="bg-container">
+        {changeFeedback ? (
+          <div className="card-container">
+            <h1 className="head-card">
+              How satisfied are you with our customer support performance
+            </h1>
+            <ul className="list-container">
+              {emojis.map(eachItem => (
+                <li key={eachItem.id} className="list-card">
+                  <button
+                    type="button"
+                    className="btn"
+                    onClick={this.showResult}
+                  >
+                    <img
+                      src={eachItem.imageUrl}
+                      alt={eachItem.name}
+                      className="image-card"
+                    />
+                    <span className="name-card">{eachItem.name}</span>
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <div className="card-container">
+            <img src={loveEmojiUrl} alt="love emoji" className="image-card" />
+            <h1 className="head-card">Thank You</h1>
+            <p className="test-card">
+              We will your feedback to improve our customer support performance
+            </p>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+export default Feedback
